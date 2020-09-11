ff6e786c735448346d5d4ca025dd0516418383f1
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-11 15:21:46 +0200

    Created assets folder & added google fonts, also created react-native.config file to run the google fonts& also updated the App component to render the images based on orientation

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
-import {SafeAreaView, Text, StyleSheet, FlatList} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+} from 'react-native';
 
 import CardImage from './Components/CardImage/CardImage';
 
 class App extends React.Component {
   state = {
     dataList: [],
+    orientation: '',
   };
   componentDidMount() {
     this.fetchData();
+    Dimensions.addEventListener('change', this.getOrientation);
   }
+
   fetchData = async () => {
     try {
       const url = 'https://randomuser.me/api/?&nat=gb&results=40';
@@ -22,15 +31,27 @@ class App extends React.Component {
     }
   };
 
+  getOrientation = () => {
+    const {width, height} = Dimensions.get('window');
+    if (width < height) {
+      this.setState({orientation: 'portrait'});
+    } else {
+      this.setState({orientation: 'landscape'});
+    }
+  };
+
   render() {
+    const {dataList, orientation} = this.state;
     return (
       <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Image Gallery</Text>
         <FlatList
           numColumns={4}
-          data={this.state.dataList}
+          data={dataList}
           keyExtractor={(item) => item.login.uuid}
-          renderItem={({item}) => <CardImage item={item} />}
+          renderItem={({item}) => (
+            <CardImage item={item} orientation={orientation} />
+          )}
         />
       </SafeAreaView>
     );
@@ -44,9 +65,10 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 23,
-    fontWeight: '700',
+    fontWeight: '500',
     marginVertical: 10,
     alignSelf: 'center',
+    fontFamily: 'Monoton-Regular',
   },
 });
 
